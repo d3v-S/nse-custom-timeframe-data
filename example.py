8e4fef14faeeafdec38789c6dc91ad5ccc3d11ba
d3v-S <d3v4shish@gmail.com>
2022-10-01 15:37:29 +0530

    added per-day-df support

--- example.py
+++ example.py
@@ -1,3 +1,25 @@
-from nse_custom_candlestick_data import ohlcDataFrames
+from nse_custom_candlestick_data import ohlcDataFrames, ohlcDataFrameForOneYear, ohlcDataFrameForOneYearPerDay
+import datetime as dt
+import pandas as pd
 
-print(ohlcDataFrames(timeframes=[1,2,3,4,5,6,7,8,9]))
+
+# list_of_df = ohlcDataFrameForOneYear()
+# print(len(list_of_df))
+
+# tf1 = list_of_df[0]
+# tf1['tmp'] = tf1.index
+# tf1[["day", "time"]] = tf1["tmp"].astype(str).str.split(expand=True)
+# df1 = tf1.groupby(["day"])
+# #l = [df1.get_group(x) for x in df1.groups]
+
+# print(df1.groups.keys())
+
+# for  x in df1.groups:
+# 	print(x)
+# 	print(df1.get_group(x))
+# 	print(type(df1.get_group(x)))
+# 	print("*************************************")
+
+import pprint
+pp = pprint.PrettyPrinter(depth=4)
+pp.pprint(ohlcDataFrameForOneYearPerDay())
